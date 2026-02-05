921a2db8f90a25ab47775560d45698e2a09dec1c
Joseph Duffy <commits@josephduffy.co.uk>
2026-02-05 18:16:25 +0000

    Broaden supported SwiftSyntax version support

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.2"),
         .package(url: "https://github.com/swiftlang/swift-subprocess.git", branch: "main"),
-        .package(url: "https://github.com/swiftlang/swift-syntax", from: "510.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "510.0.0" ..< "602.999.99"),
     ],
     targets: [
         .executableTarget(
